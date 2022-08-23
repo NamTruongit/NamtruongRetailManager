446d545b4a86da773e6f072bb30d7ae0a63108ad
NamTruongit <namtruong.it.dn@gmail.com>
2022-08-23 11:16:40 +0700

    Register DB tables
    the table for the basic register system.

---
+++ NRMData/dbo/Tables/Inventory.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Inventory]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [ProductID] INT NOT NULL, 
+    [Quantity] INT NOT NULL DEFAULT 1, 
+    [PurchasePrice] MONEY NOT NULL, 
+    [PuchaseDate] DATETIME NOT NULL DEFAULT getutcdate()
+)
